c2ff726cc69442c63291725347a90e9fff94590b
my011215 <73942702+my011215@users.noreply.github.com>
2023-03-21 21:20:18 +0800

    Update TradeService.java
    buyCurrentCompare and sellCurrentCompare have been added.

--- trade-server/src/main/java/com/example/service/TradeService.java
+++ trade-server/src/main/java/com/example/service/TradeService.java
@@ -41,6 +41,53 @@ public class TradeService {
         tradeDetailMapper.insertTradeDetail(new TradeDetail(null, "buy", bc, qc, price, size, System.currentTimeMillis(), account));
         return 1;
     }
+    
+    @Transactional(rollbackFor = Exception.class)
+    public int buyCurrentCompare(String bc, BigDecimal price) throws Exception {
+        String url1 = "https://api-aws.huobi.pro/market/history/kline?symbol=" + bc + "usdt&period=1day";
+        System.out.println(url1);
+        System.getProperties().put("proxySet", "true");
+
+        //指定代理所在的服务器
+        System.getProperties().put("proxyHost", "127.0.0.1");
+
+        //指定代理监听的端口
+        System.getProperties().put("proxyPort", "7890");
+        // 发送POST请求
+        URL url = new URL(url1);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");//修改发送方式
+        conn.setRequestProperty("Content-Type",
+                "application/json");
+        conn.setRequestProperty("Connection", "Keep-Alive");
+        conn.setUseCaches(false);
+        conn.setDoOutput(true);
+
+        // 获取响应状态
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            return 2;
+        }
+        // 获取响应内容体
+        String line, result = "";
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                conn.getInputStream(), "utf-8"));
+        while ((line = in.readLine()) != null) {
+            result += line + "\n";
+        }
+        in.close();
+        System.out.println(result);
+        JSONObject jsonObject =  JSON.parseObject(result);
+        JSONArray jsonArray = null;
+        jsonArray = jsonObject.getJSONArray("data");//获取数组
+        System.out.println(jsonArray.getJSONObject(0).get("close"));
+        BigDecimal price1 = new BigDecimal(String.valueOf(jsonArray.getJSONObject(0).get("close")));
+        System.out.println(price1);
+        BigDecimal flag1 = price1.subtract(price);
+        BigDecimal f = new BigDecimal(0.1);
+        System.out.println(f);
+        int flag2 = flag1.compareTo(f);
+        return flag2;
+    }
 
     @Transactional(rollbackFor = Exception.class)
     public int sellCurrent(String bc, String qc, BigDecimal price, BigDecimal size, String account) throws Exception {
@@ -58,6 +105,53 @@ public class TradeService {
         accountMapper.updateCurrencyBalance(qc, qcBalance.add(price.multiply(size)), account);
         tradeDetailMapper.insertTradeDetail(new TradeDetail(null, "sell", bc, qc, price, size, System.currentTimeMillis(), account));
         return 1;
+    }
+    
+     @Transactional(rollbackFor = Exception.class)
+    public int sellCurrentCompare(String bc, BigDecimal price) throws Exception {
+        String url1 = "https://api-aws.huobi.pro/market/history/kline?symbol=" + bc + "usdt&period=1day";
+        System.out.println(url1);
+        System.getProperties().put("proxySet", "true");
+
+        //指定代理所在的服务器
+        System.getProperties().put("proxyHost", "127.0.0.1");
+
+        //指定代理监听的端口
+        System.getProperties().put("proxyPort", "7890");
+        // 发送POST请求
+        URL url = new URL(url1);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");//修改发送方式
+        conn.setRequestProperty("Content-Type",
+                "application/json");
+        conn.setRequestProperty("Connection", "Keep-Alive");
+        conn.setUseCaches(false);
+        conn.setDoOutput(true);
+
+        // 获取响应状态
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            return 2;
+        }
+        // 获取响应内容体
+        String line, result = "";
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                conn.getInputStream(), "utf-8"));
+        while ((line = in.readLine()) != null) {
+            result += line + "\n";
+        }
+        in.close();
+        System.out.println(result);
+        JSONObject jsonObject =  JSON.parseObject(result);
+        JSONArray jsonArray = null;
+        jsonArray = jsonObject.getJSONArray("data");//获取数组
+        System.out.println(jsonArray.getJSONObject(0).get("close"));
+        BigDecimal price1 = new BigDecimal(String.valueOf(jsonArray.getJSONObject(0).get("close")));
+        System.out.println(price1);
+        BigDecimal flag1 = price1.subtract(price);
+        BigDecimal f = new BigDecimal(0.1);
+        System.out.println(f);
+        int flag2 = flag1.compareTo(f);
+        return flag2;
     }
 
     public List<TradeDetail> getTradeDetails(String account) {
